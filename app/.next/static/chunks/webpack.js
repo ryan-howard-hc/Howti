9d5214aced39d66859a044e3ae3ec6333c5c7781
Ryan <ryan.howard1993@gmail.com>
2023-08-15 01:03:29 +0000

    pages?

--- app/.next/static/chunks/webpack.js
+++ app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b6abd3abdf1db0f6"; }
+/******/ 		__webpack_require__.h = function() { return "a3e24f58690fbd80"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,9 +1,14 @@
-import Link from "next/link";
+import React, { useEffect } from 'react';
+import Link from 'next/link';
+import Navbar from './00header';
 
 export default function Page() {
-    return <div>
-      <h1>Home page JDH</h1>
+  return (
+    <div>
+      <Navbar />
+      <h1>Howticulture</h1>
       <Link href="/dashboard">Dashboard</Link>
-      <Link href="/about">about</Link>
+      <Link href="/about">About</Link>
     </div>
-  }
+  );
+}
